4a38ad06397fea5665e19f15b461f754437634b7
sudhanshu2070 <sssallen2016@gmail.com>
2025-04-29 04:30:48 +0530

    Strategy template 0.3

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,50 +1,46 @@
 import React from 'react';
 import BrokerConnection from '../Brokers/BrokerConnection';
-import PnLDisplay from '../PanelDisplay/PnLDisplay';
-import TradeButtons from '../TradeButtons/TradeButtons';
 import MarketSection from '../MarketSection/MarketSection';
 import RecentActivity from '../RecentActivity/RecentStrategyActivity';
 import styles from './Dashboard.module.css';
 import MeanReversionStrategy from '../StrategyTemplate/MeanReversionStrategy/MeanReversionStrategy';
 import StrangleStrategy from '../StrategyTemplate/StrangleStrategy/StrangleStrategy';
+import TradeButtons from '../TradeButtons/TradeButtons';
+import PnLDisplay from '../PanelDisplay/PnLDisplay';
 
 const Dashboard: React.FC = () => {
   return (
     <div className={styles.dashboardContainer}>
-      <header className={styles.dashboardHeader}>
-        <h1>Dashboard</h1>
-      </header>
-      
-      <div className={styles.mainContent}>
-        {/* Left column content */}
-        <div className={styles.leftColumn}>
-          <section className={styles.dashboardSection}>
-            <BrokerConnection />
-          </section>
-          
-          <div className={styles.pnlTradeContainer}>
-            <PnLDisplay />
-            <TradeButtons />
-          </div>
-          
-          <section className={styles.dashboardSection}>
-            <MarketSection />
-          </section>
-        </div>
-        
-        {/* Right column content (RecentActivity) */}
-        <div className={styles.rightColumn}>
-          <section className={styles.dashboardSection}>
-            <RecentActivity />
-          </section>
-        </div>
 
-        <div className={styles.strategySection}>
+      {/* Main Content */}
+      <div className={styles.mainContent}>
+        {/* Connected Brokers */}
+        <section className={styles.connectedBrokers}>
+          <BrokerConnection />
+        </section>
+
+        {/* PnL and Buttons
+        <section className={styles.pnlAndButtons}>
+          <PnLDisplay />
+          <TradeButtons />
+        </section> */}
+
+        {/* Strategy Templates */}
+        <section className={styles.strategyTemplates}>
           <h2 className={styles.sectionTitle}>Strategy Templates</h2>
           <div className={styles.strategyGrid}>
             <StrangleStrategy />
-            {/* <TrendFollowingStrategy /> */}
             <MeanReversionStrategy />
+          </div>
+        </section>
+
+        {/* Bottom Row: Market Watch + Recent Activity */}
+        <div className={styles.bottomRow}>
+          <div className={styles.marketWatch}>
+            <MarketSection />
+          </div>
+          <div className={styles.recentActivity}>
+            <RecentActivity />
           </div>
         </div>
       </div>
